f35280f222dc309c65779e1c34cdc3a178cced7d
Tanjim Kamal <tanjimkamal1@gmail.com>
2026-08-06 18:48:22 +0600

    Make the places where I will put the iframes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Starship vote
+This is a chrome extension for [Hack Club Stardance](https://stardane.hackclub.com/) to vote with only one tab. The demo and repo are shown in a sidebar so you don't have to open new tabs to view them.
+
+
+---
+GPLv3 Licensed, see LICENSE for more details.
---
+++ content.js
@@ -0,0 +1,110 @@
+"use strict"
+var demowrapper=0
+var repowrapper=0
+var scwrapper=0
+function switchto(x){
+    [scwrapper,demowrapper,repowrapper].forEach((e,i)=>{
+        if(i==x){e.style.display="block"}
+        else{e.style.display="none"}
+    })
+}
+function waitForElement(selector) {
+    return new Promise((resolve)=>{
+        const element=document.querySelector(selector)
+        if(element)return resolve(element)
+        const observer=new MutationObserver(()=>{
+            const el=document.querySelector(selector)
+            if(el){
+                observer.disconnect()
+                resolve(el)
+            }
+        })
+        observer.observe(document.body, { childList: true, subtree: true })
+        const CL=()=>{
+            const el=document.querySelector(selector)
+            if (el) {
+                observer.disconnect()
+                resolve(el)
+            } else {
+                requestAnimationFrame(CL)
+            }
+        }
+        requestAnimationFrame(CL)
+    })
+}
+function hoverin(x){
+    x.style.backgroundColor="#fff8d5"
+    x.style.color="#08061e"
+}
+function hoverout(x){
+    x.style.color="#fff8d5"
+    x.style.backgroundColor="#08061e"
+}
+async function main(){
+    console.log("[starship] launching...")
+    scwrapper=document.createElement("span")
+    console.log("[starship] waiting for sparkle")
+    await waitForElement(".vote-scorecard__sparkle")
+    console.log("[starship] done waiting for sparkle!")
+    const scinside=document.querySelector("form.vote-scorecard__form")
+    const wrapperwrapper=document.querySelector("section.vote-scorecard")
+    scinside.replaceWith(scwrapper)
+    scwrapper.appendChild(scinside)
+    scwrapper.id="scorecard-wrapper"
+    document.querySelectorAll(".vote-scorecard__sparkle, #vote-scorecard-title").forEach(e=>{e.remove()})
+    const xhead=document.querySelector("header.vote-scorecard__header")
+    demowrapper=document.createElement("span")
+    demowrapper.style.display="none"
+    demowrapper.innerHTML=`
+    <h1>placeholder for demo</h1>
+    `
+    wrapperwrapper.appendChild(demowrapper)
+    repowrapper=document.createElement("span")
+    repowrapper.style.display="none"
+    repowrapper.innerHTML=`
+    <h1>placeholder for repo</h1>
+    `
+    wrapperwrapper.appendChild(repowrapper)
+    xhead.style.display="flex"
+    xhead.innerHTML=`
+    <button>Scorecard</button> 
+    <button>Demo</button> 
+    <button>Repo</button>
+    `
+    xhead.querySelectorAll("button").forEach((e,i)=>{
+        e.addEventListener("click",()=>{
+            switchto(i)
+        })
+        e.style.backgroundColor="#08061e"
+        e.style.color="#fff8d5"
+        e.style.fontWeight="600"
+        e.style.border="2px solid hsla(0,0%,100%,.5)"
+        e.style.borderRadius="999px"
+        e.style.padding="15px"
+        e.style.fontSize="30px"
+        e.style.transition="background 150ms ease,border-color 150ms ease,color 150ms ease"
+        e.style.margin="auto"
+        e.style.height="100%"
+        e.style.width="auto"
+        e.addEventListener("mouseenter",()=>{
+            hoverin(e)
+        })
+        e.addEventListener("mouseleave",()=>{
+            hoverout(e)
+        })
+    })
+
+}
+main()
+let lasturl=window.location.href
+new MutationObserver(()=>{
+    const currenturl=window.location.href
+    if(lasturl!=currenturl){
+        lasturl=currenturl
+        if (currenturl.includes("/rate/new")){
+            console.log("[starship] calling main")
+            main()
+        }
+    }
+}).observe(document,{attributes: true,characterData:true,subtree:true,childList:true})
+
